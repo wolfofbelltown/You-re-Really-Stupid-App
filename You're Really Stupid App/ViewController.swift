bd926e50415e024df7dc6a6e5bf699b22e0bfd22
Warren Mui <muiw215@gmail.com>
2018-07-08 21:05:51 -0700

    Updated toggle switch.

--- You're Really Stupid App/ViewController.swift
+++ You're Really Stupid App/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBAction func SwitchTo3D(_ sender: UISwitch) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        changeText()
+        mySwitch.addTarget(self, action: #selector(switchToggled(_:)), for: UIControlEvents.valueChanged)
+    }
+    
+//    func changeText() {
+//        if mySwitch.isOn {
+//            //label.text = "Switch is on"
+//            print("SWITCH 1")
+//        } else {
+//            //label.text = "Switch is off"
+//            print("SWITCH 2")
+//        }
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
