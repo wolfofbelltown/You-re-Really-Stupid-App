815ad294f767dd284742379d208873e181f6117b
Warren Mui <muiw215@gmail.com>
2018-07-22 18:26:35 -0700

    Animated button works now.

--- You're Really Stupid App/PressMeButton.swift
+++ You're Really Stupid App/PressMeButton.swift
@@ -18,41 +18,42 @@ class PressMeButton: UIButton {
     }
     */
     
+
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        let color = UIColor.green
-//        let disabledColor = color.withAlphaComponent(0.3)
-//        let gradientColor1 = UIColor(red: 100.0 / 255.0, green: 100.0 / 255.0, blue: 100.0 / 255.0, alpha: 1).cgColor
-//        let gradientColor2 = UIColor(red: 50.0 / 255.0, green: 50.0 / 255.0, blue: 50.0 / 255.0, alpha: 1).cgColor
-//
-//        let btnFont = "Noteworthy"
-//        let bthWidth = 200
-//        let btnHeight = 60
-//
-//        self.frame.size = CGSize(width: bthWidth, height: btnHeight)
-//        self.frame.origin = CGPoint(x: (((superview?.frame.width)! / 2) - (self.frame.width / 2)), y: self.frame.origin.y)
-//
-        self.layer.cornerRadius = 10.0
-        self.clipsToBounds = true
-        self.layer.borderWidth = 3.0
+//        let color = UIColor.green
 
-        self.layer.borderColor = color.cgColor
+//        self.layer.cornerRadius = 10.0
+//        self.clipsToBounds = true
+//        self.layer.borderWidth = 3.0
 //
-//        self.setTitleColor(color, for: .normal)
-//        self.setTitleColor(disabledColor, for: .disabled)
-//        self.titleLabel?.font = UIFont(name: btnFont, size: 25)
-//        self.titleLabel?.adjustsFontSizeToFitWidth = true
-//        self.setTitle(self.titleLabel?.text?.capitalized, for: .normal)
-//
-//        let btnGradient = CAGradientLayer()
-//        btnGradient.frame = self.bounds
-//        btnGradient.colors = [gradientColor1, gradientColor2]
-//        self.layer.insertSublayer(btnGradient, at: 0)
-//
-//        self.contentEdgeInsets.bottom = 4
-//
+//        self.layer.borderColor = color.cgColor
+
         self.addTarget(self, action: #selector(onPress), for: .touchUpInside)
+        //hideButton()
     }
+    
+//    var buttonOn: Bool = false
+//
+//    func showButton() {
+//        self.alpha = 1
+//    }
+//    func hideButton() {
+//        self.alpha = 0
+//    }
+//
+//    func animateButtonPressed(sender: AnyObject){
+//
+//////        if buttonOn {
+////            hideButton()
+////            print("animate1")
+//////        } else {
+//////            showButton()
+//////            print("animate2")
+//////        }
+//
+//    }
     
     @objc func onPress() {
         print("You're Stupid!")
