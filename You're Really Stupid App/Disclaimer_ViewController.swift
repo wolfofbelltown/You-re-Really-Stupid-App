cbfc1be8dd1dde430b2ac01624286a2673b873b4
Kin <jue.kin@gmail.com>
2018-08-20 17:33:28 -0700

    Add view controllers for each screen.
    Add view controllers for each screen.

---
+++ You're Really Stupid App/Disclaimer_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Disclaimer_ViewController.swift
+//  You're Really Stupid App
+//
+//  Created by Kin Jue on 8/20/18.
+//  Copyright © 2018 Warren Mui. All rights reserved.
+//
+
+import UIKit
+
+class Disclaimer_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
